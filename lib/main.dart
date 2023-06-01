de7885f2bed1fc1a03ac9614df7d571b41f8d181
yoochanhong <chanhongy6@naver.com>
2023-06-01 18:29:49 +0900

    폴더 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:stac_flutter/screen/login_page.dart';
+import 'package:stac_flutter/screen/auth/login_page.dart';
 
 void main() => runApp(const MyApp());
 
