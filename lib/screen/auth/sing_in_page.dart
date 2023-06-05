d320f497cca6e651b992e404010ffcfae3d4fc6c
yoochanhong <chanhongy6@naver.com>
2023-06-05 15:14:09 +0900

    대충 텍스트 필드 두개 던져놓기

--- lib/screen/auth/sing_in_page.dart
+++ lib/screen/auth/sing_in_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:stac_flutter/screen/widget/custom_text_field.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextEditingController idController = TextEditingController();
+  TextEditingController pwdController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          customTextField(
+            autofocus: true,
+            hintText: "아이디를 입력해주세요",
+            controller: idController,
+            context: context,
+          ),
+          const SizedBox(height: 20),
+          customTextField(
+            autofocus: false,
+            hintText: "비밀번호를 입력해주세요",
+            controller: pwdController,
+            context: context,
+          ),
+        ],
+      ),
+    );
   }
 }
