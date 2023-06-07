c5a547bfa09790e1baea4b17c57212d354bc014e
yoochanhong <chanhongy6@naver.com>
2023-06-07 21:58:20 +0900

    플로팅버튼 추가

--- lib/screen/main/main_page.dart
+++ lib/screen/main/main_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:stac_flutter/common/common.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.18,
+        height: MediaQuery.of(context).size.width * 0.18,
+        child: FloatingActionButton(
+          backgroundColor: CustomColor.pointColor,
+          onPressed: () {},
+          child: Icon(
+            Icons.add,
+            size: MediaQuery.of(context).size.width * 0.11,
+          ),
+        ),
+      ),
+    );
   }
 }
