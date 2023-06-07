6358df64ae79666560463d86659e7aa808e5edee
yoochanhong <chanhongy6@naver.com>
2023-06-07 22:27:07 +0900

    Stack으로 감싸기
    뒷부분에 컬럼 넣어서 위로 올릴거임

--- lib/screen/main/main_page.dart
+++ lib/screen/main/main_page.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:stac_flutter/common/common.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.18,
-        height: MediaQuery.of(context).size.width * 0.18,
-        child: FloatingActionButton(
-          backgroundColor: CustomColor.pointColor,
-          onPressed: () {},
-          child: Icon(
-            Icons.add,
-            size: MediaQuery.of(context).size.width * 0.11,
+      floatingActionButton: Stack(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 700),
+            curve: Curves.easeIn,
+            width: MediaQuery.of(context).size.width * 0.18,
+            height: MediaQuery.of(context).size.width * 0.18,
+            decoration: BoxDecoration(
+              color: CustomColor.pointColor,
+              borderRadius: BorderRadius.circular(99.0),
+            ),
           ),
-        ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.18,
+            height: MediaQuery.of(context).size.width * 0.18,
+            decoration: BoxDecoration(
+              color: CustomColor.pointColor,
+              borderRadius: BorderRadius.circular(99.0),
+            ),
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+              size: MediaQuery.of(context).size.width * 0.12,
+            ),
+          ),
+        ],
       ),
     );
   }
