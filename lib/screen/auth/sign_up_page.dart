85c4fcc02bdce2fae177419c6e0179afb2e13308
yoochanhong <chanhongy6@naver.com>
2023-06-07 10:29:00 +0900

    FocusNode 제거

--- lib/screen/auth/sign_up_page.dart
+++ lib/screen/auth/sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stac_flutter/common/common.dart';
 import 'package:stac_flutter/screen/main/main_page.dart';
 import 'package:stac_flutter/screen/widget/custom_text_field.dart';
 import 'package:stac_flutter/service/auth/auth_service.dart';
@@ -14,9 +15,6 @@ class _SignUpPageState extends State<SignUpPage> {
   late TextEditingController idController;
   late TextEditingController pwdController;
   late TextEditingController pwdCheckController;
-  FocusNode firstFocusNode = FocusNode();
-  FocusNode secondFocusNode = FocusNode();
-  FocusNode thirdFocusNode = FocusNode();
 
   @override
   void initState() {
@@ -36,86 +34,62 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () => Navigator.of(context).pop(),
-          icon: Icon(
-            Icons.keyboard_arrow_left,
-            color: Colors.grey,
-            size: 45,
-          ),
-        ),
-        title: Text(
-          "회원가입",
-          style: TextStyle(color: Colors.black, fontSize: 30),
-        ),
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 100),
-                CustomTextField(
-                    controller: idController,
-                    hintText: "아이디를 입력해주세요",
-                    textInputType: TextInputType.emailAddress,
-                    focusNode: firstFocusNode,
-                    autofocus: true),
-                const SizedBox(height: 20.0),
-                CustomTextField(
-                    controller: pwdController,
-                    hintText: "비밀번호를 입력해주세요",
-                    textInputType: TextInputType.visiblePassword,
-                    focusNode: secondFocusNode,
-                    autofocus: false),
-                const SizedBox(height: 20.0),
-                CustomTextField(
-                    controller: pwdCheckController,
-                    hintText: "비밀번호를 확인해주세요",
-                    focusNode: thirdFocusNode,
-                    textInputType: TextInputType.visiblePassword,
-                    autofocus: false),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 30),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width - 40,
-                height: MediaQuery.of(context).size.height * 0.06,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff3b56d5),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(99),
-                    ),
-                  ),
-                  onPressed: () {
-                    AuthService.signUp(idController.text, pwdController.text)
-                        .then((value) {
-                      if (value == 200) {
-                        Navigator.of(context).pushAndRemoveUntil(
-                            MaterialPageRoute(builder: (context) => MainPage()),
-                            (route) => false);
-                      }
-                    });
-                  },
-                  child: Text(
-                    "바로 시작하기",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ),
-            ),
+            SizedBox(height: 100),
+            CustomTextField(
+                controller: idController,
+                hintText: "아이디를 입력해주세요",
+                textInputType: TextInputType.emailAddress,
+                autofocus: true),
+            const SizedBox(height: 20.0),
+            CustomTextField(
+                controller: pwdController,
+                hintText: "비밀번호를 입력해주세요",
+                textInputType: TextInputType.visiblePassword,
+                autofocus: false),
+            const SizedBox(height: 20.0),
+            CustomTextField(
+                controller: pwdCheckController,
+                hintText: "비밀번호를 확인해주세요",
+                textInputType: TextInputType.visiblePassword,
+                autofocus: false),
           ],
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 40),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width - 40,
+            height: MediaQuery.of(context).size.height * 0.06,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: CustomColor.pointColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(99),
+                ),
+              ),
+              onPressed: () {
+                AuthService.signUp(idController.text, pwdController.text)
+                    .then((value) {
+                  if (value == 200) {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(builder: (context) => MainPage()),
+                        (route) => false);
+                  }
+                });
+              },
+              child: Text(
+                "바로 시작하기",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
