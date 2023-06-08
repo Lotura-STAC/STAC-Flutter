37a36318ed9b1cffb153e21e82347d0fe1bd307d
yoochanhong <chanhongy6@naver.com>
2023-06-08 15:32:14 +0900

    버튼 클릭 시 뒷 배경 블러처리

--- lib/screen/main/main_page.dart
+++ lib/screen/main/main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:stac_flutter/common/common.dart';
 
@@ -19,7 +21,7 @@ class _MainPageState extends State<MainPage>
   void initState() {
     super.initState();
     controller = AnimationController(
-      duration: const Duration(milliseconds: 500),
+      duration: const Duration(milliseconds: 400),
       vsync: this,
     );
     animation = Tween<double>(
@@ -41,87 +43,100 @@ class _MainPageState extends State<MainPage>
   Widget build(BuildContext context) {
     double mosw = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(bottom: 70, right: 40),
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.bottomRight,
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 500),
-                width: MediaQuery.of(context).size.width * 0.2,
-                height: isClicked
-                    ? MediaQuery.of(context).size.width * 0.7
-                    : MediaQuery.of(context).size.width * 0.2,
-                decoration: BoxDecoration(
-                  color: CustomColor.pointColor,
-                  borderRadius: BorderRadius.circular(99.0),
-                ),
-                child: Column(
-                  children: [
-                    AnimatedContainer(
-                      duration: const Duration(milliseconds: 500),
-                      height: isClicked ? mosw * 0.08 : 0,
-                    ),
-                    AnimatedContainer(
-                      duration: const Duration(milliseconds: 500),
-                      width: isClicked ? mosw * 0.17 : mosw * 0.1,
-                      height: isClicked ? mosw * 0.17 : mosw * 0.1,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(99.0),
-                      ),
-                    ),
-                    AnimatedContainer(
-                      duration: const Duration(milliseconds: 500),
-                      height: isClicked ? mosw * 0.056 : 0,
-                    ),
-                    AnimatedContainer(
-                      duration: const Duration(milliseconds: 500),
-                      width: isClicked ? mosw * 0.17 : mosw * 0.1,
-                      height: isClicked ? mosw * 0.17 : mosw * 0.1,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(99.0),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+      body: Stack(
+        children: [
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 400),
+              color: isClicked
+                  ? Colors.grey.withOpacity(0.35)
+                  : Colors.transparent,
             ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: RotationTransition(
-                turns: animation,
-                child: Transform.rotate(
-                  angle: isClicked ? 1.5708 : 0,
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isClicked = !isClicked;
-                      });
-                      controller.reset();
-                      controller.forward();
-                    },
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.2,
-                      height: MediaQuery.of(context).size.width * 0.2,
-                      decoration: BoxDecoration(
-                        color: CustomColor.pointColor,
-                        borderRadius: BorderRadius.circular(99.0),
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: MediaQuery.of(context).size.width * 0.14,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 70, right: 40),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 400),
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    height: isClicked
+                        ? MediaQuery.of(context).size.width * 0.615
+                        : MediaQuery.of(context).size.width * 0.2,
+                    decoration: BoxDecoration(
+                      color: CustomColor.pointColor,
+                      borderRadius: BorderRadius.circular(99.0),
+                    ),
+                    child: Column(
+                      children: [
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 400),
+                          height: isClicked ? mosw * 0.02 : 0,
+                        ),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 400),
+                          width: isClicked ? mosw * 0.17 : mosw * 0.1,
+                          height: isClicked ? mosw * 0.17 : mosw * 0.1,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(99.0),
+                          ),
+                        ),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 400),
+                          height: isClicked ? mosw * 0.03 : 0,
+                        ),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 400),
+                          width: isClicked ? mosw * 0.17 : mosw * 0.1,
+                          height: isClicked ? mosw * 0.17 : mosw * 0.1,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(99.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: RotationTransition(
+                    turns: animation,
+                    child: Transform.rotate(
+                      angle: isClicked ? 1.5708 : 0,
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isClicked = !isClicked;
+                          });
+                          controller.reset();
+                          controller.forward();
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.2,
+                          height: MediaQuery.of(context).size.width * 0.2,
+                          decoration: BoxDecoration(
+                            color: CustomColor.pointColor,
+                            borderRadius: BorderRadius.circular(99.0),
+                          ),
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                            size: MediaQuery.of(context).size.width * 0.14,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
