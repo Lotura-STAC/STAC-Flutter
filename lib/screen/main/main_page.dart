e8673a7125d3c0f5431e50995b2cc79f5cb5dff9
yoochanhong <chanhongy6@naver.com>
2023-06-08 11:08:45 +0900

    버튼 위치 고정

--- lib/screen/main/main_page.dart
+++ lib/screen/main/main_page.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:stac_flutter/common/common.dart';
 
@@ -41,50 +40,59 @@ class _MainPageState extends State<MainPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: Stack(
-        children: [
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 500),
-            width: MediaQuery.of(context).size.width * 0.18,
-            height: MediaQuery.of(context).size.width * 0.18,
-            decoration: BoxDecoration(
-              color: CustomColor.pointColor,
-              borderRadius: BorderRadius.circular(99.0),
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 70, right: 40),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomRight,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                width: MediaQuery.of(context).size.width * 0.18,
+                height: isClicked
+                    ? MediaQuery.of(context).size.width * 0.6
+                    : MediaQuery.of(context).size.width * 0.18,
+                decoration: BoxDecoration(
+                  color: CustomColor.pointColor,
+                  borderRadius: BorderRadius.circular(99.0),
+                ),
+              ),
             ),
-          ),
-          RotationTransition(
-            turns: animation,
-            child: Transform.rotate(
-              angle: isClicked ? 1.5708 : 0,
-              child: GestureDetector(
-                onTap: () {
-                  if (!controller.isAnimating) {
-                    setState(() {
-                      isClicked = !isClicked;
-                    });
-                    if (isClicked) {
-                      controller.reset();
-                      controller.forward();
-                    }
-                  }
-                },
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.18,
-                  height: MediaQuery.of(context).size.width * 0.18,
-                  decoration: BoxDecoration(
-                    color: CustomColor.pointColor,
-                    borderRadius: BorderRadius.circular(99.0),
-                  ),
-                  child: Icon(
-                    Icons.add,
-                    color: Colors.white,
-                    size: MediaQuery.of(context).size.width * 0.12,
+            Align(
+              alignment: Alignment.bottomRight,
+              child: RotationTransition(
+                turns: animation,
+                child: Transform.rotate(
+                  angle: isClicked ? 1.5708 : 0,
+                  child: GestureDetector(
+                    onTap: () {
+                      if (!controller.isAnimating) {
+                        setState(() {
+                          isClicked = !isClicked;
+                        });
+                        controller.reset();
+                        controller.forward();
+                      }
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.18,
+                      height: MediaQuery.of(context).size.width * 0.18,
+                      decoration: BoxDecoration(
+                        color: CustomColor.pointColor,
+                        borderRadius: BorderRadius.circular(99.0),
+                      ),
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: MediaQuery.of(context).size.width * 0.12,
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
