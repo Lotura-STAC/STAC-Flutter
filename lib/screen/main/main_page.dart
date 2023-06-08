ff494bc720dff4e551f85f7da9c01f19ca735d0c
yoochanhong <chanhongy6@naver.com>
2023-06-08 10:21:51 +0900

    누르면 90도 회전하는 버튼 ^_^

--- lib/screen/main/main_page.dart
+++ lib/screen/main/main_page.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:stac_flutter/common/common.dart';
 
@@ -8,15 +9,42 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  bool isClicked = false;
+  late final AnimationController controller;
+
+  late final Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    animation = Tween<double>(
+      begin: 0,
+      end: -0.25,
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.linear,
+    ));
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: Stack(
         children: [
           AnimatedContainer(
-            duration: Duration(milliseconds: 700),
-            curve: Curves.easeIn,
+            duration: const Duration(milliseconds: 500),
             width: MediaQuery.of(context).size.width * 0.18,
             height: MediaQuery.of(context).size.width * 0.18,
             decoration: BoxDecoration(
@@ -24,17 +52,36 @@ class _MainPageState extends State<MainPage> {
               borderRadius: BorderRadius.circular(99.0),
             ),
           ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.18,
-            height: MediaQuery.of(context).size.width * 0.18,
-            decoration: BoxDecoration(
-              color: CustomColor.pointColor,
-              borderRadius: BorderRadius.circular(99.0),
-            ),
-            child: Icon(
-              Icons.add,
-              color: Colors.white,
-              size: MediaQuery.of(context).size.width * 0.12,
+          RotationTransition(
+            turns: animation,
+            child: Transform.rotate(
+              angle: isClicked ? 1.5708 : 0,
+              child: GestureDetector(
+                onTap: () {
+                  if (!controller.isAnimating) {
+                    setState(() {
+                      isClicked = !isClicked;
+                    });
+                    if (isClicked) {
+                      controller.reset();
+                      controller.forward();
+                    }
+                  }
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.18,
+                  height: MediaQuery.of(context).size.width * 0.18,
+                  decoration: BoxDecoration(
+                    color: CustomColor.pointColor,
+                    borderRadius: BorderRadius.circular(99.0),
+                  ),
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: MediaQuery.of(context).size.width * 0.12,
+                  ),
+                ),
+              ),
             ),
           ),
         ],
