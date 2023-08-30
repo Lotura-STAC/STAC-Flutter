72e3a2d1dafd147cd6acdc89a7900ce1a94e88e9
yoochanhong <chanhongy6@naver.com>
2023-08-30 23:10:30 +0900

    로그인 response DTO 클래스

---
+++ lib/data/dto/response/sign_in_response.dart
@@ -0,0 +1,16 @@
+import 'package:stac_flutter/domain/entity/jwt_token_entity.dart';
+
+class SignInResponse {
+  String accessToken;
+  String refreshToken;
+
+  SignInResponse({required this.accessToken, required this.refreshToken});
+
+  factory SignInResponse.fromJson(Map<String, dynamic> json) => SignInResponse(
+        accessToken: json['accessToken'],
+        refreshToken: json['refreshToken'],
+      );
+
+  JWTTokenEntity toEntity() =>
+      JWTTokenEntity(accessToken: accessToken, refreshToken: refreshToken);
+}
