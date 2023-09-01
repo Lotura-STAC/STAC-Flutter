044b5b485c9c7b5a271cf7c14ca8ee76dea390c0
yoochanhong <chanhongy6@naver.com>
2023-09-01 23:25:42 +0900

    refresh 메소드 구현

--- lib/data/repository/auth/auth_repository_impl.dart
+++ lib/data/repository/auth/auth_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:stac_flutter/data/data_source/auth/auth_data_source.dart';
+import 'package:stac_flutter/data/dto/auth/request/refresh_request.dart';
 import 'package:stac_flutter/data/dto/auth/request/sign_in_request.dart';
 import 'package:stac_flutter/data/dto/auth/request/sign_up_request.dart';
 import 'package:stac_flutter/domain/auth/entity/jwt_token_entity.dart';
@@ -16,4 +17,8 @@ class AuthRepositoryImpl implements AuthRepository {
   @override
   Future<bool> signUp(SignUpRequest signUpRequest) async =>
       await dataSource.signUp(signUpRequest);
+
+  @override
+  Future<JWTTokenEntity> refresh(RefreshRequest refreshRequest) async =>
+      await dataSource.refresh(refreshRequest);
 }
