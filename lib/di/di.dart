1c115c84a5b478eabd23e3b014854874afb0ec3d
yoochanhong <chanhongy6@naver.com>
2023-09-04 20:20:00 +0900

    의존성 주입 코드

---
+++ lib/di/di.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stac_flutter/data/data_source/auth/auth_data_source.dart';
+import 'package:stac_flutter/data/repository/auth/auth_repository_impl.dart';
+import 'package:stac_flutter/domain/auth/repository/auth_repository.dart';
+import 'package:stac_flutter/domain/auth/use_case/sign_in_use_case.dart';
+import 'package:stac_flutter/domain/auth/use_case/sign_up_use_case.dart';
+import 'package:stac_flutter/presentation/sign_in/bloc/sign_in_bloc.dart';
+import 'package:stac_flutter/presentation/sign_up/bloc/sign_up_bloc.dart';
+
+Future di() async {
+  AuthDataSource authDataSource = AuthDataSource();
+  AuthRepository authRepository =
+      AuthRepositoryImpl(dataSource: authDataSource);
+  SignInUseCase signInUseCase = SignInUseCase(repository: authRepository);
+  SignUpUseCase signUpUseCase = SignUpUseCase(repository: authRepository);
+  return [
+    BlocProvider(create: (context) => SignInBloc(signInUseCase)),
+    BlocProvider(create: (context) => SignUpBloc(signUpUseCase)),
+  ];
+}
