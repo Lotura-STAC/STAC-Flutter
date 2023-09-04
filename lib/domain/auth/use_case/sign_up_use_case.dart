a6ebfb62c9d0998e14021dfdb0b424d66c0f1afe
yoochanhong <chanhongy6@naver.com>
2023-09-04 20:20:11 +0900

    회원가입 유스케이스

---
+++ lib/domain/auth/use_case/sign_up_use_case.dart
@@ -0,0 +1,13 @@
+import 'package:stac_flutter/data/dto/auth/request/sign_up_request.dart';
+import 'package:stac_flutter/domain/auth/repository/auth_repository.dart';
+
+class SignUpUseCase {
+  final AuthRepository repository;
+
+  SignUpUseCase({required this.repository});
+
+  Future<bool> execute(SignUpRequest signUpRequest) async {
+    final response = await repository.signUp(signUpRequest);
+    return response;
+  }
+}
