f8dbed73825cd5f71a8dcc85d4fabbca2487ef57
yoochanhong <chanhongy6@naver.com>
2023-09-06 23:08:20 +0900

    화면 크기 정의

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stac_flutter/di/di.dart';
 import 'package:stac_flutter/presentation/sign_in/ui/sign_in_page.dart';
 
@@ -14,9 +15,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SignInPage(),
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      builder: (context, child) => const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SignInPage(),
+      ),
     );
   }
 }
