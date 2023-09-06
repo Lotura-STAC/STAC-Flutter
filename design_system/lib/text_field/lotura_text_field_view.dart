e5de890e377ec4310ed39e97d38b8b51d23e7f67
yoochanhong <chanhongy6@naver.com>
2023-09-06 19:09:12 +0900

    텍스트 필드 뷰 추가

--- design_system/lib/text_field/lotura_text_field_view.dart
+++ design_system/lib/text_field/lotura_text_field_view.dart
@@ -1,10 +1,37 @@
+import 'package:design_system/text_field/lotura_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoturaTextFieldView extends StatelessWidget {
-  const LoturaTextFieldView({super.key});
+  LoturaTextFieldView({super.key});
+
+  final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              Text("기본 텍스트 핖드"),
+              LoturaTextField(controller: controller),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Column(
+            children: [
+              Text("비밀번호 텍스트 필드"),
+              LoturaTextField(
+                controller: controller,
+                isPasswordTextField: true,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
