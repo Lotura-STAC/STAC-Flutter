5700bfa44d4b66732cbf89c503c937090dac1574
yoochanhong <chanhongy6@naver.com>
2023-09-06 22:04:16 +0900

    버튼 테스트뷰

--- design_system/lib/button/lotura_buttom_view.dart
+++ design_system/lib/button/lotura_buttom_view.dart
@@ -1,3 +1,6 @@
+import 'package:design_system/button/lotura_icon_button.dart';
+import 'package:design_system/button/lotura_text_button.dart';
+import 'package:design_system/color/lotura_color.dart';
 import 'package:flutter/material.dart';
 
 class LoturaButtonView extends StatelessWidget {
@@ -5,6 +8,32 @@ class LoturaButtonView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              Text("아이콘 버튼"),
+              LoturaIconButton(
+                icon: Icon(
+                  Icons.add,
+                  color: LoturaColor.white,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Column(
+            children: [
+              Text("텍스트 버튼"),
+              LoturaTextButton(),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
