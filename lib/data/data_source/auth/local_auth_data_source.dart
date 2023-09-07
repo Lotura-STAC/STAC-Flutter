579779ca2ebac8c834d4621da4681cfb9f712a11
yoochanhong <chanhongy6@naver.com>
2023-09-07 21:21:17 +0900

    로그아웃 메소드 구현

---
+++ lib/data/data_source/auth/local_auth_data_source.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class LocalAuthDateSource {
+  final _storage = const FlutterSecureStorage();
+
+  Future<void> signOut() async {
+    await _storage.deleteAll();
+  }
+}
