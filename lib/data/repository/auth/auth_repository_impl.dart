c854d612709a4b74c82dbf3d8eddf5ad97968de7
yoochanhong <chanhongy6@naver.com>
2023-09-07 21:21:12 +0900

    로그아웃 메소드 적용

--- lib/data/repository/auth/auth_repository_impl.dart
+++ lib/data/repository/auth/auth_repository_impl.dart
@@ -4,21 +4,29 @@ import 'package:stac_flutter/data/dto/auth/request/sign_in_request.dart';
 import 'package:stac_flutter/data/dto/auth/request/sign_up_request.dart';
 import 'package:stac_flutter/domain/auth/entity/jwt_token_entity.dart';
 import 'package:stac_flutter/domain/auth/repository/auth_repository.dart';
+import 'package:stac_flutter/data/data_source/auth/local_auth_data_source.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
-  final RemoteAuthDataSource dataSource;
+  final RemoteAuthDataSource remoteAuthDataSource;
+  final LocalAuthDateSource localAuthDateSource;
 
-  AuthRepositoryImpl({required this.dataSource});
+  AuthRepositoryImpl(
+      {required this.remoteAuthDataSource, required this.localAuthDateSource});
 
   @override
   Future<JWTTokenEntity> signIn(SignInRequest signInRequest) async =>
-      await dataSource.signIn(signInRequest);
+      await remoteAuthDataSource.signIn(signInRequest);
 
   @override
   Future<bool> signUp(SignUpRequest signUpRequest) async =>
-      await dataSource.signUp(signUpRequest);
+      await remoteAuthDataSource.signUp(signUpRequest);
 
   @override
   Future<JWTTokenEntity> refresh(RefreshRequest refreshRequest) async =>
-      await dataSource.refresh(refreshRequest);
+      await remoteAuthDataSource.refresh(refreshRequest);
+
+  @override
+  Future<void> signOut() async {
+    await localAuthDateSource.signOut();
+  }
 }
