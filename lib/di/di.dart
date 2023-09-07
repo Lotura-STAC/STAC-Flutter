6bc62b91aaa5292799aef0c30b2c753a456577da
yoochanhong <chanhongy6@naver.com>
2023-09-07 21:20:54 +0900

    local auth data source 의존성 주입

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stac_flutter/data/data_source/auth/local_auth_data_source.dart';
 import 'package:stac_flutter/data/data_source/auth/remote_auth_data_source.dart';
 import 'package:stac_flutter/data/repository/auth/auth_repository_impl.dart';
 import 'package:stac_flutter/domain/auth/repository/auth_repository.dart';
@@ -8,9 +9,11 @@ import 'package:stac_flutter/presentation/sign_in/bloc/sign_in_bloc.dart';
 import 'package:stac_flutter/presentation/sign_up/bloc/sign_up_bloc.dart';
 
 Future<List<BlocProvider>> di() async {
-  RemoteAuthDataSource authDataSource = RemoteAuthDataSource();
-  AuthRepository authRepository =
-      AuthRepositoryImpl(dataSource: authDataSource);
+  RemoteAuthDataSource remoteAuthDataSource = RemoteAuthDataSource();
+  LocalAuthDateSource localAuthDateSource = LocalAuthDateSource();
+  AuthRepository authRepository = AuthRepositoryImpl(
+      remoteAuthDataSource: remoteAuthDataSource,
+      localAuthDateSource: localAuthDateSource);
   SignInUseCase signInUseCase = SignInUseCase(repository: authRepository);
   SignUpUseCase signUpUseCase = SignUpUseCase(repository: authRepository);
   return [
