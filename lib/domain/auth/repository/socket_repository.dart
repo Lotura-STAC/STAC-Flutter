93425c959985cc31d21c33fd0a62181d04923cd4
yoochanhong <chanhongy6@naver.com>
2023-09-08 23:42:44 +0900

    소켓 레포지토리 파라미터 추가

--- lib/domain/auth/repository/socket_repository.dart
+++ lib/domain/auth/repository/socket_repository.dart
@@ -1,11 +1,14 @@
+import 'package:stac_flutter/data/dto/auth/request/add_device_request.dart';
+import 'package:stac_flutter/data/dto/auth/request/get_user_device_list_request.dart';
+import 'package:stac_flutter/data/dto/auth/request/remove_device_request.dart';
 import 'package:stac_flutter/data/dto/auth/response/get_user_device_list_response.dart';
 
 abstract class SocketRepository {
-  Stream<GetUserDeviceListResponse> get applyStream;
+  Stream<GetUserDeviceListResponse> get userDeviceListStream;
 
-  void addDevice();
+  void addDevice(AddDeviceRequest addDeviceRequest);
 
-  void removeDevice();
+  void removeDevice(RemoveDeviceRequest removeDeviceRequest);
 
-  void getUserDeviceList();
+  void getUserDeviceList(GetUserDeviceListRequest getUserDeviceListRequest);
 }
