25340999bbab6e3b42e817d273cb13fc53304ba6
yoochanhong <chanhongy6@naver.com>
2023-09-08 23:46:53 +0900

    생성자에 소켓 로그인 코드 추가

--- lib/data/data_source/socket/remote_socket_data_source.dart
+++ lib/data/data_source/socket/remote_socket_data_source.dart
@@ -4,6 +4,7 @@ import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:stac_flutter/data/dto/auth/request/add_device_request.dart';
 import 'package:stac_flutter/data/dto/auth/request/get_user_device_list_request.dart';
 import 'package:stac_flutter/data/dto/auth/request/remove_device_request.dart';
+import 'package:stac_flutter/data/dto/auth/request/socket_login_request.dart';
 import 'package:stac_flutter/data/dto/auth/response/get_user_device_list_response.dart';
 import 'package:stac_flutter/secret.dart';
 
@@ -15,11 +16,14 @@ class RemoteSocketDataSource {
           .enableForceNewConnection()
           .build());
 
-  final StreamController<GetUserDeviceListResponse> _streamController;
+  late final StreamController<GetUserDeviceListResponse> _streamController;
 
   RemoteSocketDataSource(
-      {required StreamController<GetUserDeviceListResponse> streamController})
-      : _streamController = streamController;
+      {required StreamController<GetUserDeviceListResponse> streamController,
+      required SocketLoginRequest socketLoginRequest}) {
+    _streamController = streamController;
+    _socket.emit('Socket_login', socketLoginRequest);
+  }
 
   Stream<GetUserDeviceListResponse> get userDeviceListStream =>
       _streamController.stream.asBroadcastStream();
