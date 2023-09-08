aae9e044f16b51f752b3f2bc1be2e91ec7257c64
yoochanhong <chanhongy6@naver.com>
2023-09-08 23:42:55 +0900

    장치 추가 유스케이스

---
+++ lib/domain/auth/use_case/add_device_use_case.dart
@@ -0,0 +1,13 @@
+import 'package:stac_flutter/data/dto/auth/request/add_device_request.dart';
+import 'package:stac_flutter/domain/auth/repository/socket_repository.dart';
+
+class AddDeviceUseCase {
+  final SocketRepository _repository;
+
+  AddDeviceUseCase({required SocketRepository repository})
+      : _repository = repository;
+
+  void execute(AddDeviceRequest addDeviceRequest) {
+    _repository.addDevice(addDeviceRequest);
+  }
+}
