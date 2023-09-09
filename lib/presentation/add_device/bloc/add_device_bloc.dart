7aef2e204194470a6d23d48edeefda527d8bd1ff
yoochanhong <chanhongy6@naver.com>
2023-09-09 11:34:37 +0900

    장치 추가 블록

---
+++ lib/presentation/add_device/bloc/add_device_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stac_flutter/domain/auth/use_case/add_device_use_case.dart';
+import 'package:stac_flutter/presentation/add_device/bloc/add_device_event.dart';
+import 'package:stac_flutter/presentation/add_device/bloc/add_device_state.dart';
+
+class AddDeviceBloc extends Bloc<AddDeviceEvent, AddDeviceState> {
+  final AddDeviceUseCase _addDeviceUseCase;
+
+  AddDeviceBloc(this._addDeviceUseCase) : super(Empty()) {
+    on<AddDevice>(_addDeviceHandler);
+  }
+
+  void _addDeviceHandler(AddDevice event, Emitter<AddDeviceState> emit) {
+    try {
+      emit(Loading());
+      _addDeviceUseCase.execute(event.addDeviceRequest);
+      emit(Loaded());
+    } catch (e) {
+      emit(Error(message: e.toString()));
+    }
+  }
+}
