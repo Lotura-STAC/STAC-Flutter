d07815309e711710360d99be107f367e8cf4c8c1
yoochanhong <chanhongy6@naver.com>
2023-09-09 11:14:57 +0900

    장치 추가 이벤트

---
+++ lib/presentation/add_device/bloc/add_device_event.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:stac_flutter/data/dto/auth/request/add_device_request.dart';
+
+abstract class AddDeviceEvent extends Equatable {}
+
+class AddDevice extends AddDeviceEvent {
+  final AddDeviceRequest addDeviceRequest;
+
+  AddDevice({required this.addDeviceRequest});
+
+  @override
+  List<Object?> get props => [addDeviceRequest];
+}
