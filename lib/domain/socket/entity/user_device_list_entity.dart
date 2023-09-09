6b07067f74182ab9047281a51debdcee784bb5da
yoochanhong <chanhongy6@naver.com>
2023-09-09 23:30:48 +0900

    유저 기기 리스트 엔티티 추가

---
+++ lib/domain/socket/entity/user_device_list_entity.dart
@@ -0,0 +1,10 @@
+class UserDeviceEntity {
+  String deviceNo;
+  String deviceType;
+  int currStatus;
+
+  UserDeviceEntity(
+      {required this.deviceNo,
+      required this.deviceType,
+      required this.currStatus});
+}
