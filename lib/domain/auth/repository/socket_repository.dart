a9e642a9002697a5cfbcb19f13ebd2445235fbdf
yoochanhong <chanhongy6@naver.com>
2023-09-09 11:28:21 +0900

    소켓 로그인 메소드 추가

--- lib/domain/auth/repository/socket_repository.dart
+++ lib/domain/auth/repository/socket_repository.dart
@@ -1,10 +1,13 @@
 import 'package:stac_flutter/data/dto/auth/request/add_device_request.dart';
 import 'package:stac_flutter/data/dto/auth/request/get_user_device_list_request.dart';
 import 'package:stac_flutter/data/dto/auth/request/remove_device_request.dart';
+import 'package:stac_flutter/data/dto/auth/request/socket_login_request.dart';
 import 'package:stac_flutter/data/dto/auth/response/get_user_device_list_response.dart';
 
 abstract class SocketRepository {
   Stream<GetUserDeviceListResponse> get userDeviceListStream;
+
+  void socketLogin(SocketLoginRequest socketLoginRequest);
 
   void addDevice(AddDeviceRequest addDeviceRequest);
 
