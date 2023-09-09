ebb20dd05edb1deab4151deec00e9fb88b5fe563
yoochanhong <chanhongy6@naver.com>
2023-09-09 11:14:34 +0900

    소켓 레포지토리 구현체

--- lib/data/repository/socket/socket_repository_impl.dart
+++ lib/data/repository/socket/socket_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:stac_flutter/data/data_source/socket/local_socket_data_source.dart';
 import 'package:stac_flutter/data/data_source/socket/remote_socket_data_source.dart';
 import 'package:stac_flutter/data/dto/auth/request/add_device_request.dart';
 import 'package:stac_flutter/data/dto/auth/request/get_user_device_list_request.dart';
@@ -6,27 +7,36 @@ import 'package:stac_flutter/data/dto/auth/response/get_user_device_list_respons
 import 'package:stac_flutter/domain/auth/repository/socket_repository.dart';
 
 class SocketRepositoryImpl implements SocketRepository {
-  final RemoteSocketDataSource _dataSource;
+  final RemoteSocketDataSource _remoteSocketDataSource;
+  final LocalSocketDataSource _localSocketDataSource;
 
-  SocketRepositoryImpl({required RemoteSocketDataSource dataSource})
-      : _dataSource = dataSource;
+  SocketRepositoryImpl(
+      {required RemoteSocketDataSource remoteSocketDataSource,
+      required LocalSocketDataSource localSocketDataSource})
+      : _remoteSocketDataSource = remoteSocketDataSource,
+        _localSocketDataSource = localSocketDataSource;
 
   @override
   void addDevice(AddDeviceRequest addDeviceRequest) {
-    _dataSource.addDevice(addDeviceRequest);
+    _remoteSocketDataSource.addDevice(addDeviceRequest);
   }
 
   @override
   void getUserDeviceList(GetUserDeviceListRequest getUserDeviceListRequest) {
-    _dataSource.getUserDeviceList(getUserDeviceListRequest);
+    _remoteSocketDataSource.getUserDeviceList(getUserDeviceListRequest);
   }
 
   @override
   void removeDevice(RemoveDeviceRequest removeDeviceRequest) {
-    _dataSource.removeDevice(removeDeviceRequest);
+    _remoteSocketDataSource.removeDevice(removeDeviceRequest);
   }
 
   @override
   Stream<GetUserDeviceListResponse> get userDeviceListStream =>
-      _dataSource.userDeviceListStream;
+      _remoteSocketDataSource.userDeviceListStream;
+
+  @override
+  Future<void> saveDeviceName(String name, deviceNum) async {
+    await _localSocketDataSource.saveDeviceName(name, deviceNum);
+  }
 }
