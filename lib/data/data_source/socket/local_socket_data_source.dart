c131a338ab6784c86e8cb75aaba12c7f8e7da20e
yoochanhong <chanhongy6@naver.com>
2023-09-09 12:11:55 +0900

    토큰 불러오기, 유저 아이디 저장 & 불러오기 메소드 추가

--- lib/data/data_source/socket/local_socket_data_source.dart
+++ lib/data/data_source/socket/local_socket_data_source.dart
@@ -6,4 +6,18 @@ class LocalSocketDataSource {
   Future<void> saveDeviceName(String name, deviceNum) async {
     await _storage.write(key: deviceNum, value: name);
   }
+
+  Future<void> saveUserId(String userId) async {
+    await _storage.write(key: 'usreId', value: userId);
+  }
+
+  Future<String> getToken() async {
+    final accessToken = await _storage.read(key: 'accessToken');
+    return accessToken!;
+  }
+
+  Future<String> getUserId() async {
+    final userId = await _storage.read(key: 'userId');
+    return userId!;
+  }
 }
