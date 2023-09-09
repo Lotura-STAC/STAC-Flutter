2af172723a6c8afe957a9c8b9756f37508623f26
yoochanhong <chanhongy6@naver.com>
2023-09-09 15:16:28 +0900

    auth 데이터 소스로 아이디 토큰 저장 로직 이동

--- lib/data/data_source/auth/local_auth_data_source.dart
+++ lib/data/data_source/auth/local_auth_data_source.dart
@@ -1,9 +1,20 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:stac_flutter/domain/auth/entity/jwt_token_entity.dart';
 
 class LocalAuthDateSource {
   final _storage = const FlutterSecureStorage();
 
   Future<void> signOut() async {
     await _storage.deleteAll();
+  }
+
+  Future<void> saveUserId(String userId) async {
+    await _storage.write(key: 'userId', value: userId);
+  }
+
+  Future<void> saveToken(JWTTokenEntity jwtTokenEntity) async {
+    await _storage.write(key: 'accessToken', value: jwtTokenEntity.accessToken);
+    await _storage.write(
+        key: 'refreshToken', value: jwtTokenEntity.refreshToken);
   }
 }
