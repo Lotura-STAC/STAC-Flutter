95c052087546a2254e63cfefe9595d03fdd1c2d9
yoochanhong <chanhongy6@naver.com>
2023-09-12 07:34:10 +0900

    장치 추가 헤더 토큰 추가

--- lib/data/add_device/data_source/remote_add_device_data_source.dart
+++ lib/data/add_device/data_source/remote_add_device_data_source.dart
@@ -1,13 +1,20 @@
 import 'dart:convert';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:stac_flutter/data/add_device/dto/request/add_device_request.dart';
 import 'package:stac_flutter/secret.dart';
 
 class RemoteAddDeviceDataSource {
+  final _storage = const FlutterSecureStorage();
+
   Future<bool> addDevice(AddDeviceRequest addDeviceRequest) async {
+    final token = await _storage.read(key: 'accessToken');
     final response = await http.post(Uri.parse("$baseUrl/add_device"),
-        headers: <String, String>{"Content-Type": "application/json"},
+        headers: <String, String>{
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $token"
+        },
         body: jsonEncode(addDeviceRequest.toJson()));
     if (response.statusCode == 400) {
       return false;
