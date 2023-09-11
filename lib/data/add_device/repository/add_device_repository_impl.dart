512632a2e983ba66770d822dd260e44eb7e30a46
yoochanhong <chanhongy6@naver.com>
2023-09-12 07:23:59 +0900

    장치 추가 레포지토리 구현체

--- lib/data/add_device/repository/add_device_repository_impl.dart
+++ lib/data/add_device/repository/add_device_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:stac_flutter/data/add_device/dto/request/add_device_request.dart';
 import 'package:stac_flutter/domain/add_device/repository/add_device_repository.dart';
-import 'package:stac_flutter/data/add_device/data_source/remote_socket_data_source.dart';
+import 'package:stac_flutter/data/add_device/data_source/remote_add_device_data_source.dart';
 
 class AddDeviceRepositoryImpl implements AddDeviceRepository {
   final RemoteAddDeviceDataSource _remoteAddDeviceDataSource;
