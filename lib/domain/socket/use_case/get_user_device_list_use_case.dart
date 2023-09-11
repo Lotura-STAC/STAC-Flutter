d4102ddca6442806782c9e1c03e4a413c9945b0a
yoochanhong <chanhongy6@naver.com>
2023-09-12 07:37:35 +0900

    불필요한 import 삭제

--- lib/domain/socket/use_case/get_user_device_list_use_case.dart
+++ lib/domain/socket/use_case/get_user_device_list_use_case.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:stac_flutter/data/socket/dto/request/get_user_device_list_request.dart';
-import 'package:stac_flutter/data/socket/dto/request/socket_login_request.dart';
 import 'package:stac_flutter/data/socket/dto/response/get_user_device_list_response.dart';
 import 'package:stac_flutter/domain/socket/repository/socket_repository.dart';
 
--- lib/presentation/main/bloc/main_state.dart
+++ lib/presentation/main/bloc/main_state.dart
@@ -1,6 +1,5 @@
 import 'package:equatable/equatable.dart';
 import 'package:stac_flutter/data/socket/dto/response/get_user_device_list_response.dart';
-import 'package:stac_flutter/domain/socket/entity/user_device_list_entity.dart';
 
 abstract class MainState extends Equatable {}
 
