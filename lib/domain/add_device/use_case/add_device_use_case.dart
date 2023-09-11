1fbd8fc9ae2c93aad6e4e82ace8863243668bf45
yoochanhong <chanhongy6@naver.com>
2023-09-12 07:24:35 +0900

    AuthRepository => AddDeviceRepository

--- lib/domain/add_device/use_case/add_device_use_case.dart
+++ lib/domain/add_device/use_case/add_device_use_case.dart
@@ -1,20 +1,20 @@
 import 'package:stac_flutter/data/add_device/dto/request/add_device_request.dart';
 import 'package:stac_flutter/data/socket/dto/request/get_user_device_list_request.dart';
-import 'package:stac_flutter/domain/auth/repository/auth_repository.dart';
+import 'package:stac_flutter/domain/add_device/repository/add_device_repository.dart';
 import 'package:stac_flutter/domain/socket/repository/socket_repository.dart';
 
 class AddDeviceUseCase {
-  final AuthRepository _authRepository;
+  final AddDeviceRepository _addDeviceRepository;
   final SocketRepository _socketRepository;
 
   AddDeviceUseCase(
-      {required AuthRepository authRepository,
+      {required AddDeviceRepository addDeviceRepository,
       required SocketRepository socketRepository})
-      : _authRepository = authRepository,
+      : _addDeviceRepository = addDeviceRepository,
         _socketRepository = socketRepository;
 
   void execute(AddDeviceRequest addDeviceRequest) {
-    _authRepository.addDevice(addDeviceRequest);
+    _addDeviceRepository.addDevice(addDeviceRequest);
     Future.delayed(const Duration(milliseconds: 500)).then((value) {
       _socketRepository.getUserDeviceList(
           GetUserDeviceListRequest(userId: "", accessToken: ""));
