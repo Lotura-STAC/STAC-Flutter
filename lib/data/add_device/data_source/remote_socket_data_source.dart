7434c650339b40bada48de7d4ff7aa998e6ce260
yoochanhong <chanhongy6@naver.com>
2023-09-11 23:01:10 +0900

    장치 추가 메소드

---
+++ lib/data/add_device/data_source/remote_socket_data_source.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:stac_flutter/data/add_device/dto/request/add_device_request.dart';
+import 'package:stac_flutter/secret.dart';
+
+class RemoteSocketDataSource {
+  Future<bool> addDevice(AddDeviceRequest addDeviceRequest) async {
+    final response = await http.post(Uri.parse("$baseUrl/add_device"),
+        headers: <String, String>{"Content-Type": "application/json"},
+        body: jsonEncode(addDeviceRequest.toJson()));
+    if (response.statusCode == 400) {
+      return false;
+    }
+    return true;
+  }
+}
