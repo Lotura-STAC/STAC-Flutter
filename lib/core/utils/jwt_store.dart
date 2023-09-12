191e902904b52d1bcd417580ce70d4d513ffebfa
yoochanhong <chanhongy6@naver.com>
2023-09-12 13:11:28 +0900

    jwt store

---
+++ lib/core/utils/jwt_store.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class JWTStore {
+  static const _storage = FlutterSecureStorage();
+
+  static Future<String> getAccessToken() async {
+    final accessToken = await _storage.read(key: 'accessToken') ?? "";
+    return accessToken;
+  }
+
+  static Future<String> getRefreshToken() async {
+    final refreshToken = await _storage.read(key: 'refreshToken') ?? "";
+    return refreshToken;
+  }
+
+  static Future<void> setToken(String accessToken, refreshToken) async {
+    await _storage.write(key: 'accessToken', value: accessToken);
+    await _storage.write(key: 'refreshToken', value: refreshToken);
+  }
+}
