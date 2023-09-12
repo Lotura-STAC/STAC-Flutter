f0fc57f61809f154fffe8ab87c444b79fcd5d9b8
yoochanhong <chanhongy6@naver.com>
2023-09-12 22:08:55 +0900

    리스트 타일 적용

--- lib/presentation/main/ui/main_page.dart
+++ lib/presentation/main/ui/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:design_system/color/lotura_color.dart';
+import 'package:design_system/list_tile/lotura_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -33,50 +34,55 @@ class _MainPageState extends State<MainPage> {
         elevation: 0.0,
         actions: [
           IconButton(
-              padding: EdgeInsets.only(right: 30.0.r),
-              onPressed: () {},
-              icon: Icon(
-                Icons.settings,
-                color: LoturaColor.black,
-              )),
+            padding: EdgeInsets.only(right: 30.0.r),
+            onPressed: () {},
+            icon: Icon(
+              Icons.settings,
+              color: LoturaColor.black,
+            ),
+          ),
         ],
       ),
       body: Padding(
         padding: EdgeInsets.only(left: 24.0.r, right: 24.0.r),
         child: Center(
-          child: BlocBuilder<MainBloc, MainState>(builder: (context, state) {
-            if (state is Loading) {
-              return CircularProgressIndicator();
-            }
-            if (state is Loaded) {
-              return SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.7,
-                child: ListView.builder(
-                  itemCount: state.list.list.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.4,
-                          height: 100.0.r,
-                          child: Text(state.list.list[index].deviceNo),
-                        ),
-                        SizedBox(height: 20.0.r),
-                      ],
-                    );
-                  },
-                ),
-              );
-            }
-            return Container();
-          }),
+          child: BlocBuilder<MainBloc, MainState>(
+            builder: (context, state) {
+              if (state is Loading) {
+                return const CircularProgressIndicator();
+              }
+              if (state is Loaded) {
+                if (state.list.list.isEmpty) {
+                  Container();
+                } else {
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    child: ListView.builder(
+                      itemCount: state.list.list.length,
+                      itemBuilder: (context, index) {
+                        return LoturaListTile(
+                          width: 382.0.r,
+                          height: 78.0.r,
+                          deviceType: state.list.list[index].deviceType,
+                          text: state.list.list[index].name,
+                          status: state.list.list[index].currStatus,
+                          margin: EdgeInsets.all(8.0.r),
+                        );
+                      },
+                    ),
+                  );
+                }
+              }
+              return Container();
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => AddDevicePage())),
-        child: Icon(Icons.add),
+        onPressed: () => Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const AddDevicePage())),
+        child: const Icon(Icons.add),
       ),
     );
   }
