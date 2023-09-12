086a197079d4eeda1f58ff6d94d3925960461582
yoochanhong <chanhongy6@naver.com>
2023-09-12 16:55:03 +0900

    장치 제거 이벤트 추가

--- lib/presentation/main/bloc/main_bloc.dart
+++ lib/presentation/main/bloc/main_bloc.dart
@@ -2,12 +2,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stac_flutter/domain/socket/use_case/get_user_device_list_use_case.dart';
 import 'package:stac_flutter/presentation/main/bloc/main_event.dart';
 import 'package:stac_flutter/presentation/main/bloc/main_state.dart';
+import 'package:stac_flutter/domain/remove_device/use_case/remove_device_use_case.dart';
 
 class MainBloc extends Bloc<MainEvent, MainState> {
   final GetUserDeviceListUseCase _getUserDeviceListUseCase;
+  final RemoveDeviceUseCase _removeDeviceUseCase;
 
-  MainBloc(this._getUserDeviceListUseCase) : super(Empty()) {
+  MainBloc(this._getUserDeviceListUseCase, this._removeDeviceUseCase)
+      : super(Empty()) {
     on<GetUserDeviceListEvent>(_getUserDeviceListEventHandler);
+    on<RemoveDeviceEvent>(_removeDeviceEventHandler);
     on<ModifyDeviceNameEvent>(_modifyDeviceEventHandler);
   }
 
@@ -19,6 +23,17 @@ class MainBloc extends Bloc<MainEvent, MainState> {
       await for (var a in _getUserDeviceListUseCase.userDeviceList) {
         emit(Loaded(list: a));
       }
+    } catch (e) {
+      emit(Error(message: e.toString()));
+    }
+  }
+
+  void _removeDeviceEventHandler(
+      RemoveDeviceEvent event, Emitter<MainState> emit) async {
+    try {
+      emit(Loading());
+      final response =
+          await _removeDeviceUseCase.execute(event.removeDeviceRequest);
     } catch (e) {
       emit(Error(message: e.toString()));
     }
--- lib/presentation/main/bloc/main_event.dart
+++ lib/presentation/main/bloc/main_event.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:stac_flutter/data/remove_device/dto/remove_device_request.dart';
 import 'package:stac_flutter/data/socket/dto/request/get_user_device_list_request.dart';
 
 abstract class MainEvent extends Equatable {}
@@ -13,6 +14,15 @@ class GetUserDeviceListEvent extends MainEvent {
 }
 
 class ModifyDeviceNameEvent extends MainEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+class RemoveDeviceEvent extends MainEvent {
+  final RemoveDeviceRequest removeDeviceRequest;
+
+  RemoveDeviceEvent({required this.removeDeviceRequest});
+
   @override
   List<Object?> get props => [];
 }
