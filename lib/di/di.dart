4aa7b806708367ef1a5fcbd5629d8d0d84902907
yoochanhong <chanhongy6@naver.com>
2023-09-12 13:21:24 +0900

    local socket data source 삭제

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -5,17 +5,14 @@ import 'package:stac_flutter/data/add_device/repository/add_device_repository_im
 import 'package:stac_flutter/data/auth/data_source/local_auth_data_source.dart';
 import 'package:stac_flutter/data/auth/data_source/remote_auth_data_source.dart';
 import 'package:stac_flutter/data/auth/repository/auth_repository_impl.dart';
-import 'package:stac_flutter/data/socket/data_source/local_socket_data_source.dart';
 import 'package:stac_flutter/data/socket/data_source/remote_socket_data_source.dart';
 import 'package:stac_flutter/data/socket/dto/response/get_user_device_list_response.dart';
 import 'package:stac_flutter/data/socket/repository/socket_repository_impl.dart';
 import 'package:stac_flutter/domain/add_device/repository/add_device_repository.dart';
 import 'package:stac_flutter/domain/auth/repository/auth_repository.dart';
-import 'package:stac_flutter/domain/socket/entity/user_device_list_entity.dart';
 import 'package:stac_flutter/domain/socket/repository/socket_repository.dart';
 import 'package:stac_flutter/domain/add_device/use_case/add_device_use_case.dart';
 import 'package:stac_flutter/domain/socket/use_case/get_user_device_list_use_case.dart';
-import 'package:stac_flutter/domain/socket/use_case/remove_device_use_case.dart';
 import 'package:stac_flutter/domain/auth/use_case/sign_in_use_case.dart';
 import 'package:stac_flutter/domain/auth/use_case/sign_up_use_case.dart';
 import 'package:stac_flutter/presentation/add_device/bloc/add_device_bloc.dart';
@@ -31,14 +28,12 @@ Future<List<BlocProvider>> di() async {
       RemoteAddDeviceDataSource();
 
   LocalAuthDateSource localAuthDateSource = LocalAuthDateSource();
-  LocalSocketDataSource localSocketDataSource = LocalSocketDataSource();
 
   AuthRepository authRepository = AuthRepositoryImpl(
       remoteAuthDataSource: remoteAuthDataSource,
       localAuthDateSource: localAuthDateSource);
-  SocketRepository socketRepository = SocketRepositoryImpl(
-      remoteSocketDataSource: remoteSocketDataSource,
-      localSocketDataSource: localSocketDataSource);
+  SocketRepository socketRepository =
+      SocketRepositoryImpl(remoteSocketDataSource: remoteSocketDataSource);
 
   AddDeviceRepository addDeviceRepository = AddDeviceRepositoryImpl(
       remoteAddDeviceDataSource: remoteAddDeviceDataSource);
@@ -48,8 +43,6 @@ Future<List<BlocProvider>> di() async {
       socketRepository: socketRepository);
   GetUserDeviceListUseCase getUserDeviceListUseCase =
       GetUserDeviceListUseCase(repository: socketRepository);
-  RemoveDeviceUseCase removeDeviceUseCase =
-      RemoveDeviceUseCase(repository: socketRepository);
   SignInUseCase signInUseCase = SignInUseCase(repository: authRepository);
   SignUpUseCase signUpUseCase = SignUpUseCase(repository: authRepository);
 
@@ -59,7 +52,6 @@ Future<List<BlocProvider>> di() async {
     BlocProvider<AddDeviceBloc>(
         create: (context) => AddDeviceBloc(addDeviceUseCase)),
     BlocProvider<MainBloc>(
-        create: (context) =>
-            MainBloc(removeDeviceUseCase, getUserDeviceListUseCase)),
+        create: (context) => MainBloc(getUserDeviceListUseCase))
   ];
 }
