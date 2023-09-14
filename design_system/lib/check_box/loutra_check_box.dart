9111c9ddd974f1b58aa7b80f3ae51f927aa866e4
yoochanhong <chanhongy6@naver.com>
2023-09-14 19:45:11 +0900

    lotura check box

---
+++ design_system/lib/check_box/loutra_check_box.dart
@@ -0,0 +1,40 @@
+import 'package:design_system/color/lotura_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoturaCheckBox extends StatefulWidget {
+  const LoturaCheckBox({
+    super.key,
+    required this.isSelected,
+    this.onPressed,
+    this.width,
+    this.height,
+  });
+
+  final bool isSelected;
+  final void Function()? onPressed;
+  final double? width, height;
+
+  @override
+  State<LoturaCheckBox> createState() => _LoturaCheckBoxState();
+}
+
+class _LoturaCheckBoxState extends State<LoturaCheckBox> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onPressed,
+      child: Container(
+        width: widget.width ?? 40.0.r,
+        height: widget.height ?? 40.0.r,
+        decoration: BoxDecoration(
+          color: widget.isSelected == true
+              ? LoturaColor.gray100
+              : LoturaColor.primary900,
+          border: Border.all(color: LoturaColor.gray200, width: 3),
+          borderRadius: const BorderRadius.all(Radius.circular(99)),
+        ),
+      ),
+    );
+  }
+}
