656f2a822ea4d698de84ab795bbedfb5ff7b24a9
yoochanhong <chanhongy6@naver.com>
2023-09-14 21:42:42 +0900

    장치 추가 후 리스트 요청

--- lib/domain/add_device/use_case/add_device_use_case.dart
+++ lib/domain/add_device/use_case/add_device_use_case.dart
@@ -1,13 +1,22 @@
 import 'package:stac_flutter/data/add_device/dto/request/add_device_request.dart';
+import 'package:stac_flutter/data/socket/dto/request/get_user_device_list_request.dart';
 import 'package:stac_flutter/domain/add_device/repository/add_device_repository.dart';
+import 'package:stac_flutter/domain/socket/repository/socket_repository.dart';
 
 class AddDeviceUseCase {
   final AddDeviceRepository _addDeviceRepository;
+  final SocketRepository _socketRepository;
 
-  AddDeviceUseCase({required AddDeviceRepository addDeviceRepository})
-      : _addDeviceRepository = addDeviceRepository;
+  AddDeviceUseCase(
+      {required AddDeviceRepository addDeviceRepository,
+      required SocketRepository socketRepository})
+      : _addDeviceRepository = addDeviceRepository,
+        _socketRepository = socketRepository;
 
   Future<bool> execute(AddDeviceRequest addDeviceRequest) async {
-    return await _addDeviceRepository.addDevice(addDeviceRequest);
+    final response = await _addDeviceRepository.addDevice(addDeviceRequest);
+    _socketRepository
+        .getUserDeviceList(GetUserDeviceListRequest(accessToken: ""));
+    return response;
   }
 }
