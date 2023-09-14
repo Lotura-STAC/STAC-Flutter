04dea7d53733610ffe1c86c7a6ac2abca5853a58
yoochanhong <chanhongy6@naver.com>
2023-09-15 00:52:34 +0900

    알림 신청 레포지토리 구현체

---
+++ lib/data/notify/repository/notify_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:stac_flutter/data/notify/data_source/remote_notify_data_source.dart';
+import 'package:stac_flutter/data/notify/dto/request/notify_request.dart';
+import 'package:stac_flutter/domain/notify/repository/notify_repository.dart';
+
+class NotifyRepositoryImpl extends NotifyRepository {
+  final RemoteNotifyDataSource _remoteNotifyDataSource;
+
+  NotifyRepositoryImpl({required RemoteNotifyDataSource remoteNotifyDataSource})
+      : _remoteNotifyDataSource = remoteNotifyDataSource;
+
+  @override
+  Future<bool> notify(NotifyRequest notifyRequest) async {
+    return await _remoteNotifyDataSource.notify(notifyRequest);
+  }
+}
