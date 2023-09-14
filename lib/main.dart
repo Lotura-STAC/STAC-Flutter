67ebb9c115389efc9e2bac3639162c76aa23f520
yoochanhong <chanhongy6@naver.com>
2023-09-14 23:22:39 +0900

    fcminit 함수 실행

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,17 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stac_flutter/core/utils/fcm_init.dart';
 import 'package:stac_flutter/di/di.dart';
+import 'package:stac_flutter/firebase_options.dart';
 import 'package:stac_flutter/presentation/sign_in/ui/sign_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final blocs = await di();
   HttpOverrides.global = NoCheckCertificateHttpOverrides();
   runApp(MultiBlocProvider(providers: blocs, child: const MyApp()));
@@ -20,10 +26,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(430, 932),
-      builder: (context, child) => const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SignInPage(),
-      ),
+      builder: (context, child) {
+        fcmInit(context);
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SignInPage(),
+        );
+      },
     );
   }
 }
