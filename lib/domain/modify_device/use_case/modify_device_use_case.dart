3a6c322073be23c3c31af2d64bfb1d6b624f05a5
yoochanhong <chanhongy6@naver.com>
2023-09-14 21:56:53 +0900

    장치 수정 후 딜레이 삭제

--- lib/domain/modify_device/use_case/modify_device_use_case.dart
+++ lib/domain/modify_device/use_case/modify_device_use_case.dart
@@ -1,13 +1,25 @@
+import 'dart:io';
+
 import 'package:stac_flutter/data/modify_device/dto/request/modify_device_request.dart';
+import 'package:stac_flutter/data/socket/dto/request/get_user_device_list_request.dart';
 import 'package:stac_flutter/domain/modify_device/repository/modify_device_repository.dart';
+import 'package:stac_flutter/domain/socket/repository/socket_repository.dart';
 
 class ModifyDeviceUseCase {
   final ModifyDeviceRepository _modifyDeviceRepository;
+  final SocketRepository _socketRepository;
 
-  ModifyDeviceUseCase({required ModifyDeviceRepository modifyDeviceRepository})
-      : _modifyDeviceRepository = modifyDeviceRepository;
+  ModifyDeviceUseCase(
+      {required ModifyDeviceRepository modifyDeviceRepository,
+      required SocketRepository socketRepository})
+      : _modifyDeviceRepository = modifyDeviceRepository,
+        _socketRepository = socketRepository;
 
   Future<bool> execute(ModifyDeviceRequest modifyDeviceRequest) async {
-    return await _modifyDeviceRepository.modifyDevice(modifyDeviceRequest);
+    final response =
+        await _modifyDeviceRepository.modifyDevice(modifyDeviceRequest);
+    _socketRepository
+        .getUserDeviceList(GetUserDeviceListRequest(accessToken: ""));
+    return response;
   }
 }
