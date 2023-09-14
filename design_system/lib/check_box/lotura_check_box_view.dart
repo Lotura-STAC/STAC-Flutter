7516ecf51d082ebe3d2d06f3783fcb363c7a58bc
yoochanhong <chanhongy6@naver.com>
2023-09-14 19:45:18 +0900

    체크 박스 테스트 뷰

--- design_system/lib/check_box/lotura_check_box_view.dart
+++ design_system/lib/check_box/lotura_check_box_view.dart
@@ -1,10 +1,27 @@
+import 'package:design_system/check_box/loutra_check_box.dart';
+import 'package:design_system/color/lotura_color.dart';
 import 'package:flutter/material.dart';
 
-class LoturaCheckBoxView extends StatelessWidget {
+class LoturaCheckBoxView extends StatefulWidget {
   const LoturaCheckBoxView({super.key});
 
   @override
+  State<LoturaCheckBoxView> createState() => _LoturaCheckBoxViewState();
+}
+
+class _LoturaCheckBoxViewState extends State<LoturaCheckBoxView> {
+  bool isSelected = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: LoturaColor.gray100,
+      body: Center(
+        child: LoturaCheckBox(
+          isSelected: isSelected,
+          onPressed: () => setState(() => isSelected = !isSelected),
+        ),
+      ),
+    );
   }
 }
