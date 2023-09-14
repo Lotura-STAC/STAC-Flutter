bba5ef01b682d0498237b4808f27715184fb5904
yoochanhong <chanhongy6@naver.com>
2023-09-15 00:52:28 +0900

    알림 신청 데이터 소스

---
+++ lib/data/notify/data_source/remote_notify_data_source.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:stac_flutter/core/utils/jwt_store.dart';
+import 'package:stac_flutter/data/notify/dto/request/notify_request.dart';
+import 'package:stac_flutter/secret.dart';
+import 'package:http/http.dart' as http;
+
+class RemoteNotifyDataSource {
+  Future<bool> notify(NotifyRequest notifyRequest) async {
+    final token = await JWTStore.getAccessToken();
+    final response = await http.post(Uri.parse("$baseUrl/notify_me"),
+        headers: <String, String>{
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $token"
+        },
+        body: jsonEncode(notifyRequest.toJson()));
+    if (response.statusCode == 400) {
+      return false;
+    }
+    return true;
+  }
+}
