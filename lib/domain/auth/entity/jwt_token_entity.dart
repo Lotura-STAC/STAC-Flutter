e1a22727c3def50613ec840e82ecfdfc9cbddbef
yoochanhong <chanhongy6@naver.com>
2023-09-15 22:27:20 +0900

    role 추가

--- lib/domain/auth/entity/jwt_token_entity.dart
+++ lib/domain/auth/entity/jwt_token_entity.dart
@@ -1,6 +1,12 @@
+import 'package:stac_flutter/data/auth/dto/response/sign_in_response.dart';
+
 class JWTTokenEntity {
   String accessToken;
   String refreshToken;
+  Role role;
 
-  JWTTokenEntity({required this.accessToken, required this.refreshToken});
+  JWTTokenEntity(
+      {required this.accessToken,
+      required this.refreshToken,
+      required this.role});
 }
