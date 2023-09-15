9a6a8088472b822bf85d4473f5eb8b63efa0320c
yoochanhong <chanhongy6@naver.com>
2023-09-15 22:27:00 +0900

    role 추가

--- lib/data/auth/dto/response/refresh_response.dart
+++ lib/data/auth/dto/response/refresh_response.dart
@@ -1,17 +1,23 @@
+import 'package:stac_flutter/data/auth/dto/response/sign_in_response.dart';
 import 'package:stac_flutter/domain/auth/entity/jwt_token_entity.dart';
 
 class RefreshResponse {
   String accessToken;
   String refreshToken;
+  Role role;
 
-  RefreshResponse({required this.accessToken, required this.refreshToken});
+  RefreshResponse(
+      {required this.accessToken,
+      required this.refreshToken,
+      required this.role});
 
   factory RefreshResponse.fromJson(Map<String, dynamic> json) =>
       RefreshResponse(
         accessToken: json['accessToken'],
         refreshToken: json['refreshToken'],
+        role: json['role'] == Role.admin.name ? Role.admin : Role.guest,
       );
 
-  JWTTokenEntity toEntity() =>
-      JWTTokenEntity(accessToken: accessToken, refreshToken: refreshToken);
+  JWTTokenEntity toEntity() => JWTTokenEntity(
+      accessToken: accessToken, refreshToken: refreshToken, role: role);
 }
