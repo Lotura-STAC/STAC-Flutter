c525c051b3a25155f409e5c3ff243e4f89d39b51
yoochanhong <chanhongy6@naver.com>
2023-09-16 22:40:19 +0900

    의존성 새로 주입

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -7,6 +7,8 @@ import 'package:stac_flutter/data/auth/data_source/remote_auth_data_source.dart'
 import 'package:stac_flutter/data/auth/repository/auth_repository_impl.dart';
 import 'package:stac_flutter/data/modify_device/data_source/remote_modify_device_data_source.dart';
 import 'package:stac_flutter/data/modify_device/repository/modify_device_repository_impl.dart';
+import 'package:stac_flutter/data/notify/data_source/remote_notify_data_source.dart';
+import 'package:stac_flutter/data/notify/repository/notify_repository_impl.dart';
 import 'package:stac_flutter/data/remove_device/data_source/remote_remove_device_data_source.dart';
 import 'package:stac_flutter/data/remove_device/repository/remove_device_repository_impl.dart';
 import 'package:stac_flutter/data/socket/data_source/remote_socket_data_source.dart';
@@ -16,6 +18,8 @@ import 'package:stac_flutter/domain/add_device/repository/add_device_repository.
 import 'package:stac_flutter/domain/auth/repository/auth_repository.dart';
 import 'package:stac_flutter/domain/modify_device/repository/modify_device_repository.dart';
 import 'package:stac_flutter/domain/modify_device/use_case/modify_device_use_case.dart';
+import 'package:stac_flutter/domain/notify/repository/notify_repository.dart';
+import 'package:stac_flutter/domain/notify/use_case/notify_use_case.dart';
 import 'package:stac_flutter/domain/remove_device/repository/remove_device_repository.dart';
 import 'package:stac_flutter/domain/remove_device/use_case/remove_device_use_case.dart';
 import 'package:stac_flutter/domain/socket/repository/socket_repository.dart';
@@ -39,7 +43,7 @@ Future<List<BlocProvider>> di() async {
       RemoteRemoveDeviceDataSource();
   RemoteModifyDeviceDataSource remoteModifyDeviceDataSource =
       RemoteModifyDeviceDataSource();
-
+  RemoteNotifyDataSource remoteNotifyDataSource = RemoteNotifyDataSource();
   LocalAuthDateSource localAuthDateSource = LocalAuthDateSource();
 
   AuthRepository authRepository = AuthRepositoryImpl(
@@ -53,6 +57,8 @@ Future<List<BlocProvider>> di() async {
       remoteAddDeviceDataSource: remoteAddDeviceDataSource);
   ModifyDeviceRepository modifyDeviceRepository = ModifyDeviceRepositoryImpl(
       remoteModifyDeviceDataSource: remoteModifyDeviceDataSource);
+  NotifyRepository notifyRepository =
+      NotifyRepositoryImpl(remoteNotifyDataSource: remoteNotifyDataSource);
 
   AddDeviceUseCase addDeviceUseCase = AddDeviceUseCase(
       addDeviceRepository: addDeviceRepository,
@@ -66,14 +72,16 @@ Future<List<BlocProvider>> di() async {
   ModifyDeviceUseCase modifyDeviceUseCase = ModifyDeviceUseCase(
       modifyDeviceRepository: modifyDeviceRepository,
       socketRepository: socketRepository);
+  NotifyUseCase notifyUseCase =
+      NotifyUseCase(notifyRepository: notifyRepository);
   return [
     BlocProvider<SignInBloc>(create: (context) => SignInBloc(signInUseCase)),
     BlocProvider<SignUpBloc>(create: (context) => SignUpBloc(signUpUseCase)),
     BlocProvider<AddDeviceBloc>(
         create: (context) => AddDeviceBloc(addDeviceUseCase)),
     BlocProvider<MainBloc>(
-        create: (context) =>
-            MainBloc(getUserDeviceListUseCase, removeDeviceUseCase)),
+        create: (context) => MainBloc(
+            getUserDeviceListUseCase, removeDeviceUseCase, notifyUseCase)),
     BlocProvider<ModifyDeviceBloc>(
         create: (context) => ModifyDeviceBloc(modifyDeviceUseCase)),
   ];
