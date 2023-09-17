000f90449b40c0b95b8e2fc1b664480c190b4b12
yoochanhong <chanhongy6@naver.com>
2023-09-17 18:23:25 +0900

    notify admin 추가

--- lib/domain/notify/repository/notify_repository.dart
+++ lib/domain/notify/repository/notify_repository.dart
@@ -1,5 +1,8 @@
+import 'package:stac_flutter/data/notify/dto/request/notify_admin_request.dart';
 import 'package:stac_flutter/data/notify/dto/request/notify_request.dart';
 
 abstract class NotifyRepository {
   Future<bool> notify(NotifyRequest notifyRequest);
+
+  Future<bool> notifyAdmin(NotifyAdminRequest notifyAdminRequest);
 }
