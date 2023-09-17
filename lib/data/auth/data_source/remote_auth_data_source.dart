d538a2a96104b35012a936df636219e000ed02fa
yoochanhong <chanhongy6@naver.com>
2023-09-17 21:19:06 +0900

    로그아웃 api 추가

--- lib/data/auth/data_source/remote_auth_data_source.dart
+++ lib/data/auth/data_source/remote_auth_data_source.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:stac_flutter/core/utils/jwt_store.dart';
 import 'package:stac_flutter/data/auth/dto/request/refresh_request.dart';
 import 'package:stac_flutter/data/auth/dto/request/sign_in_request.dart';
 import 'package:stac_flutter/data/auth/dto/request/sign_up_request.dart';
@@ -40,5 +41,15 @@ class RemoteAuthDataSource {
       throw Exception(response.body);
     }
     return RefreshResponse.fromJson(jsonDecode(response.body)).toEntity();
+  }
+
+  Future<bool> signOut() async {
+    final token = await JWTStore.getAccessToken();
+    final response = await http.post(Uri.parse("$baseUrl/no_notify_admin"),
+        headers: <String, String>{"Authorization": "Bearer $token"});
+    if (response.statusCode == 400) {
+      throw Exception("로그아웃 실패");
+    }
+    return true;
   }
 }
