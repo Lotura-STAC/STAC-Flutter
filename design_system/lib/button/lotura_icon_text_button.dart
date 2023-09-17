67d6010c328b6e1566fe895972f2aef8ec4cc6f2
yoochanhong <chanhongy6@naver.com>
2023-09-17 13:14:27 +0900

    lotura icon text button

---
+++ design_system/lib/button/lotura_icon_text_button.dart
@@ -0,0 +1,43 @@
+import 'package:design_system/color/lotura_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoturaIconTextButton extends StatelessWidget {
+  const LoturaIconTextButton({
+    super.key,
+    this.width,
+    this.height,
+    required this.text,
+    required this.icon,
+    this.function,
+    this.color,
+  });
+
+  final double? width, height;
+  final Icon icon;
+  final Color? color;
+  final Text text;
+  final void Function()? function;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: function,
+      child: Container(
+        width: width ?? 150.0.r,
+        height: height ?? 80.0.r,
+        decoration: BoxDecoration(
+          color: color ?? LoturaColor.gray200,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon,
+            text,
+          ],
+        ),
+      ),
+    );
+  }
+}
