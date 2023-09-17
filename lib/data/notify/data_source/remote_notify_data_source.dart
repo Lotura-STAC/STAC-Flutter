eea812a2e70f78fcef5cbe5e50a04a8ed516a9fd
yoochanhong <chanhongy6@naver.com>
2023-09-17 18:22:48 +0900

    admin 1회용 devicetoken 보내기 data source

--- lib/data/notify/data_source/remote_notify_data_source.dart
+++ lib/data/notify/data_source/remote_notify_data_source.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:stac_flutter/core/utils/jwt_store.dart';
+import 'package:stac_flutter/data/notify/dto/request/notify_admin_request.dart';
 import 'package:stac_flutter/data/notify/dto/request/notify_request.dart';
 import 'package:stac_flutter/secret.dart';
 import 'package:http/http.dart' as http;
@@ -17,6 +18,18 @@ class RemoteNotifyDataSource {
     if (response.statusCode == 400) {
       return false;
     }
+    return true;
+  }
+
+  Future<bool> notifyAdmin(NotifyAdminRequest notifyAdminRequest) async {
+    final token = await JWTStore.getAccessToken();
+    final response = await http.post(Uri.parse("$baseUrl/notify_me_admin"),
+        headers: <String, String>{
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $token"
+        },
+        body: jsonEncode(notifyAdminRequest.toJson()));
+    if (response.statusCode == 400) return false;
     return true;
   }
 }
