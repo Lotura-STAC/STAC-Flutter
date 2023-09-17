bed6f422d7e1a67c2a27ddeb3149d024c6bf6456
yoochanhong <chanhongy6@naver.com>
2023-09-17 13:14:22 +0900

    뷰에 아이콘 텍스트 버튼 추가

--- design_system/lib/button/lotura_buttom_view.dart
+++ design_system/lib/button/lotura_buttom_view.dart
@@ -1,4 +1,5 @@
 import 'package:design_system/button/lotura_icon_button.dart';
+import 'package:design_system/button/lotura_icon_text_button.dart';
 import 'package:design_system/button/lotura_text_button.dart';
 import 'package:design_system/color/lotura_color.dart';
 import 'package:flutter/material.dart';
@@ -9,30 +10,44 @@ class LoturaButtonView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            children: [
-              Text("아이콘 버튼"),
-              LoturaIconButton(
-                icon: Icon(
-                  Icons.add,
-                  color: LoturaColor.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Text("아이콘 버튼"),
+                LoturaIconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: LoturaColor.white,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Column(
-            children: [
-              Text("텍스트 버튼"),
-              LoturaTextButton(),
-            ],
-          ),
-        ],
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Column(
+              children: [
+                Text("텍스트 버튼"),
+                LoturaTextButton(),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Column(
+              children: [
+                Text("아이콘 + 텍스트 버튼"),
+                LoturaIconTextButton(
+                  icon: Icon(Icons.add),
+                  text: Text("더하기"),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
