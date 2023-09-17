8fa0b9d9bb81e1998389b4bcecbbb149550f6760
yoochanhong <chanhongy6@naver.com>
2023-09-17 16:52:21 +0900

    아이콘 표시 뷰

--- design_system/lib/icon/lotura_icon_view.dart
+++ design_system/lib/icon/lotura_icon_view.dart
@@ -1,3 +1,4 @@
+import 'package:design_system/icon/lotura_icons.dart';
 import 'package:flutter/material.dart';
 
 class LoturaIconView extends StatelessWidget {
@@ -5,6 +6,21 @@ class LoturaIconView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(LoturaIcons.laundry),
+            Icon(LoturaIcons.dry),
+            Icon(LoturaIcons.working),
+            Icon(LoturaIcons.list),
+            Icon(LoturaIcons.breakdown),
+            Icon(LoturaIcons.checkCircle),
+            Icon(LoturaIcons.grid),
+          ],
+        ),
+      ),
+    );
   }
 }
